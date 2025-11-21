99505231e35324494b6febd15f6cc8ee1a937adc
carlosj2073 <carlosjaramillo2073@gmail.com>
2025-11-20 19:36:14 -0500

    Project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+## Instructions
+
+# structure 
+- fred_data folder: houses all of the fred_csv files
+- project.py : the python code for the project. (so far I read and joined all of the files so that they are in one dataframe)
+- environment.yml : the environment settings
+
+# Environment
+- Run the following in your terminal: conda env create -f environment.yml
+
+- make sure the environment is active: conda activate (the environment name). 
+    - (This is so the libraries we use are the same so we dont run into individual problems with dependencies). 
+
+
+
---
+++ project.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from scipy.optimize import minimize
+import datetime
+
+# pandas settings for display of dataframes
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+# Lists assets and attatches corresponding indexes
+asset_files = {
+    'UK' : 'fred_data/NASDAQNQGBN_PC1.csv',
+    'Japan' : 'fred_data/NASDAQNQJPN_PC1.csv',
+    'Canada' : 'fred_data/NASDAQNQCAN_PC1.csv',
+    'US' : 'fred_data/NASDAQCOM_PC1.csv'
+}
+
+# Creates dictionary to hold each dataframe
+df_list = {}
+
+# Reads the csvs for each asset and assigns columns with correct column name
+for asset_name, filename in asset_files.items():
+    df = pd.read_csv(
+        filename,
+        index_col = 0,
+        parse_dates = True
+    )
+
+    df.columns = [asset_name]
+    df_list[asset_name] = df
+
+# Joins the tables for each asset into one
+prices_df = pd.concat(df_list.values(), axis=1, join='outer')
+prices_df = prices_df.dropna(how='all')
+
+# prints the first and last 5 rows
+print("Joined Price Data")
+print(prices_df.head())
+print(prices_df.tail())
